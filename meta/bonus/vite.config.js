445cddc019154d412333fce1b08122ebd3fb9977
Witek Socha <witek.soch@gmail.com>
2023-05-04 21:20:50 +0200

    add bonus stage

---
+++ meta/bonus/vite.config.js
@@ -0,0 +1,17 @@
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+
+import { defineConfig } from "vite";
+import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
+
+export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      legalComments: "none",
+    },
+    entries: "./index.html"
+  },
+  plugins: [
+    ckeditor5({ theme: require.resolve("@ckeditor/ckeditor5-theme-lark") }),
+  ],
+});
