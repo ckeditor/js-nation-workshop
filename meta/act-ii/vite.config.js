0bb8e94745a94c4226011e98bc4785111371c70c
Witek Socha <witek.soch@gmail.com>
2023-05-16 14:28:47 +0200

    fix Vite (global is not available by default)

--- meta/act-ii/vite.config.js
+++ meta/act-ii/vite.config.js
@@ -6,7 +6,10 @@ import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
 
 export default defineConfig({
   optimizeDeps: {
-    entries: "./index.html"
+    entries: "./index.html",
+  },
+  define: {
+    global: "window",
   },
   plugins: [
     ckeditor5({ theme: require.resolve("@ckeditor/ckeditor5-theme-lark") }),
